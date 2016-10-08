6de84b74274d78c5197caa9f1f3cf77dfaba2a47
lzm420241 <lzm420241@gmail.com>
2016-10-08 20:54:12 +0800

    build easy react project

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+/* from https://www.codementor.io/reactjs/tutorial/beginner-guide-setup-reactjs-environment-npm-babel-6-webpack */
+
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+var APP_DIR = path.resolve(__dirname, 'src/client/app');
+
+var config = {
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module : {
+    loaders : [
+      {
+        test : /\.jsx?/,
+        include : APP_DIR,
+        loader : 'babel'
+      }
+    ]
+  }
+};
+
+module.exports = config;
